8c47ecde85d3dd4ab6173db1a381af42d39f4f39
VladimirFabri98 <vfficial98@gmail.com>
2024-10-24 15:45:22 +0200

    Vezbe 3 - 24.10.2024. Izmenjeno

---
+++ ObjektnoIM/src/vezbe/TestingClass.java
@@ -0,0 +1,117 @@
+package vezbe;
+
+import geometry.Point;
+
+public class TestingClass {
+	public static void main(String[] args) {
+	/*	System.out.println("Hello World!");
+		
+		int prviBroj = 55;
+		double decimalniBroj = 20.50;
+		System.out.println(prviBroj + decimalniBroj);
+		System.out.println(prviBroj - decimalniBroj);
+		System.out.println(prviBroj * decimalniBroj);
+		System.out.println(prviBroj / decimalniBroj);
+		
+//		int drugiBroj = 40.40;
+//		double drugiDecimalni = 20;
+//		boolean tacnost = false;
+//		String tekst = "Pozdrav studentima menadzmenta";
+		//char jedanKarakter = 'a';
+		
+		int dnevnaTemperatura = 24;
+		int jutarnjaTemperatura = 7;
+		String testTekst = "Danas je suncano";
+		boolean tacnost = false;
+		
+		int razlika = dnevnaTemperatura - jutarnjaTemperatura;
+		System.out.println("Danasnja razlika u temperaturi je: " + razlika);
+		System.out.println(testTekst + " sa " + dnevnaTemperatura + " stepena celzijusa");
+		System.out.println(testTekst + ":" + tacnost);
+		
+		dnevnaTemperatura = 31;
+		jutarnjaTemperatura = 10;
+		
+		if((dnevnaTemperatura-jutarnjaTemperatura) > 20) {
+			System.out.println("Primer ispis 1");
+		}else {
+			System.out.println("Primer ispis 2");
+		}
+		
+		if(dnevnaTemperatura > 30) {
+			System.out.println("Primer ispis 3");
+		}else {
+			System.out.println("Primer ispis 4");
+		}
+		
+//		ZADATAK 3 - PETLJE
+		
+		int zbir = 0;
+		for(int i = 0; i<=200; i+=2) {
+			zbir = zbir+i;
+//			zbir += i;
+		}
+		System.out.println(zbir);
+		int brojac = 0;
+		zbir = 0;
+		while(brojac<=200) {
+			if(brojac%2 == 0) {
+				zbir += brojac;
+			}
+			brojac++;
+		}
+		System.out.println(zbir);
+		
+//		ZADATAK 4 - PETLJE
+		int brojPrestupnihGodina = 0;
+		for(int godina = 2024; godina <=2100; godina+=4) {
+			if(godina == 2024) {
+				continue;
+			}
+			if(godina  % 4 == 0) {
+				if(godina % 100 == 0) {
+					if(godina % 400 == 0) {
+						brojPrestupnihGodina++;
+					}
+				}
+				else {
+					brojPrestupnihGodina++;
+				}
+			}
+		}
+		
+		System.out.println(brojPrestupnihGodina);
+		*/
+		
+		/*==============================
+		 * =============================
+		 * 			VEZBE 3
+		 *============================== 
+		 */
+		
+		int broj = 20;
+		Point p1 = new Point();
+//		System.out.println(p1);
+//		System.out.println("Vrednost x koordinate je: " + p1.x);
+//		System.out.println("Vrednost y koordinate je: " + p1.y);
+//		System.out.println("Vrednost selected koordinate je: " + p1.selected);
+//		p1.setX(50);
+//		p1.setY(10);
+//		p1.setSelected(true);
+		
+		Point p2 = new Point();
+//		p1 = p2;
+//		System.out.println("Vrednost x koordinate tacke p1 je: " + p1.x);
+//		p1.setX(50);
+//		p2.setX(60);
+//		System.out.println("Vrednost x koordinate p2 je: " + p2.x);
+//		System.out.println("Vrednost x koordinate p1 je: " + p1.x);
+
+//		p1.setX(20);	
+		
+//		System.out.println(p1.distance(p2));
+		
+		
+		
+	}
+}
